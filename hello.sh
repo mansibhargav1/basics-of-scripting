b4f8fb2e2d7e9febee2a6bd93adea48e2619a120
mansibhargav1 <mansibhargav2023@gmail.com>
2026-03-07 23:56:41 +0530

    Print hello and write message in a file

---
+++ hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hello Dosto"
+
+echo "Welcome to DevOps - Zero To Hero"
+
+echo "we are learning git"
+
+date
+
+whoami
---
+++ save-message/save-message.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Enter your message:"
+read message
+
+echo "$message" > message.txt
+
+echo "Message saved to message.txt"
